3042e4de8244067af9fe90f16b3dda88da597c4b
Sudhir Rao <sudrao@gmail.com>
2011-09-25 11:32:08 -0700

    Test for search query.

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -21,6 +21,23 @@ describe Tweet do
   it "saves a valid tweet" do
     Tweet.create(text: "Hello!", when: DateTime.now, location: {:lat => 10.0, :lng => 10.0}).should have(:no).errors
   end
+  
+  def make_tweets(count=1, lat=10.0, lng=10.0)
+    count.times do |i|
+      Tweet.create(text: "Hello #{i}!", when: DateTime.now, location: {:lat => lat, :lng => lng})
+    end
+  end
+  
+  it "finds nearby tweets" do
+    lat = 34.0
+    lng = -74.0
+    make_tweets(10, lat, lng)
+    make_tweets(10, lat+20, lng-20) # far away
+    # look for slightly different lat/lng
+    tweets = Tweet.where(:location.near => [lng-1, lat+1]).order('when DESC').limit(50).all
+    tweets.length.should be(10)
+  end
+  
 end
 
     
